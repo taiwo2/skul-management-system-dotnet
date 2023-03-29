61f6e9d49f5061363f572f0abb2aa8af7f49496f
taiwo2 <adetonat@ymail.com>
2023-03-29 16:46:55 +0100

    Add class and emrollment table

--- Data/SchoolManagementDbContext.cs
+++ Data/SchoolManagementDbContext.cs
@@ -11,7 +11,11 @@ public partial class SchoolManagementDbContext : DbContext
     {
     }
 
+    public virtual DbSet<Class> Classes { get; set; }
+
     public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<Enrollment> Enrollments { get; set; }
 
     public virtual DbSet<Lecturer> Lecturers { get; set; }
 
@@ -19,19 +23,47 @@ public partial class SchoolManagementDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Class>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Classes__3214EC070372FA58");
+
+            entity.HasOne(d => d.Course).WithMany(p => p.Classes)
+                .HasForeignKey(d => d.CourseId)
+                .HasConstraintName("FK__Classes__CourseI__2D27B809");
+
+            entity.HasOne(d => d.Lecturer).WithMany(p => p.Classes)
+                .HasForeignKey(d => d.LecturerId)
+                .HasConstraintName("FK__Classes__Lecture__2C3393D0");
+        });
+
         modelBuilder.Entity<Course>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Courses__3214EC07AA407006");
+            entity.HasKey(e => e.Id).HasName("PK__Courses__3214EC071D89EC86");
 
-            entity.HasIndex(e => e.Code, "UQ__Courses__A25C5AA79E28C10F").IsUnique();
+            entity.HasIndex(e => e.Code, "UQ__Courses__A25C5AA71DB2DD3F").IsUnique();
 
             entity.Property(e => e.Code).HasMaxLength(5);
             entity.Property(e => e.Name).HasMaxLength(50);
         });
 
+        modelBuilder.Entity<Enrollment>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Enrollme__3214EC07D267AE70");
+
+            entity.Property(e => e.Grade).HasMaxLength(2);
+
+            entity.HasOne(d => d.Class).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.ClassId)
+                .HasConstraintName("FK__Enrollmen__Class__30F848ED");
+
+            entity.HasOne(d => d.Student).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.StudentId)
+                .HasConstraintName("FK__Enrollmen__Stude__300424B4");
+        });
+
         modelBuilder.Entity<Lecturer>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Lecturer__3214EC0792430EED");
+            entity.HasKey(e => e.Id).HasName("PK__Lecturer__3214EC076B11DBBB");
 
             entity.Property(e => e.FirstName).HasMaxLength(50);
             entity.Property(e => e.LastName).HasMaxLength(50);
@@ -39,7 +71,7 @@ public partial class SchoolManagementDbContext : DbContext
 
         modelBuilder.Entity<Student>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC0731CE1159");
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC0783BFCF67");
 
             entity.Property(e => e.DateOfBirth).HasColumnType("date");
             entity.Property(e => e.FirstName).HasMaxLength(50);
---
+++ Models/LecturerMetadata.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testproject.Data;
+
+public class LecturerMetadata
+
+{
+  [Display(Name = "First Name")]
+  public string FirstName { get; set; } = null!;
+ 
+  [Display(Name = "Last Name")]
+  public string LastName { get; set; } = null!;
+
+}
+
+[ModelMetadataType(typeof(LecturerMetadata))]
+public partial class Lecturer{}
